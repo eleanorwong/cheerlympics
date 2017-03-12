0f9dae1d97515b2fce3e7fd565b2f4d1ede016bc
Eleanor Wong <eleanorwong@live.ca>
2017-03-11 20:50:24 -0800

    Color and Text

--- js/app.js
+++ js/app.js
@@ -17,3 +17,41 @@ cheerlympics.controller('CheerlympicsController',
         $scope.temp = "Hello2!"
     }
 );
+
+cheerlympics.controller('EditController', ['$scope',
+    function EditController($scope) {
+        $scope.tool = '';
+
+        $scope.line1 = "";
+        $scope.line2 = "";
+
+        $scope.color = "white";
+
+        $scope.sounds = [{name: "Applause"}, {name: "Horn"}, { name: "Cheering"}];
+
+        $scope.setTool = function(newTool) {
+            if($scope.tool === newTool) {
+                $scope.tool = '';
+            } else {
+                $scope.tool = newTool;
+            }
+        }
+
+        $scope.isSet = function(toolName) {
+            return $scope.tool === toolName;
+        }
+
+        $scope.isSingleLine = function() {
+            if($scope.text.line1 === "" && $scope.text.line2 === "") {
+                return false;
+            }
+
+            return true;
+        }
+
+        $scope.setColor = function(color) {
+            $scope.color = color;
+            $scope.tool = '';
+        }
+    }
+]);
